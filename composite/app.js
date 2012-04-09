f653cbf25f474fe2c5555b7b7bab207ca6154223
Phips Peter <pspeter333@gmail.com>
2012-04-09 00:12:50 -0500

    Updated the composite to be responsive

--- composite/app.js
+++ composite/app.js
@@ -1,7 +1,7 @@
 $(function() {
   $("#comp").mousemove(function(e) {
-    var x = (e.pageX - this.offsetLeft) / 5.6;
-    var y = (e.pageY - this.offsetTop) / 4.35;
+    var x = (100 * (e.pageX - this.offsetLeft)) / $("#comp").width();
+    var y = (100 * (e.pageY - this.offsetTop)) / $("#comp").height();
     $("#zoom").css("background-position", "" + x + "% " + y + "%");
   });
 });
